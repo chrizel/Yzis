e734f714a41b9da7e8cac323e2db85e525eb41ee
Mickael Marchand <mikmak@freenux.org>
2003-06-01 00:28:09 +0000

    still preparing : commands ...

--- yz_view.cpp
+++ yz_view.cpp
@@ -72,6 +72,12 @@ void YZView::sendChar( QChar c) {
 		case YZ_VIEW_MODE_COMMAND:
 			/* will be handled after the switch */
 			break;
+		case YZ_VIEW_MODE_EX:
+			if ( c.unicode() == 13 ) {// <ENTER> 
+				//try to execute that stuff :)
+
+			} // else nothing :)
+			break;
 		default:
 			/* ?? */
 			//printf("Currently unknown MODE\n");

db6ab53eb57db9ee59fbc01f60f176fd6bd817e4
Mickael Marchand <mikmak@freenux.org>
2004-03-30 20:40:55 +0000

    Search support through the / command just a copy of EX mode handling in libyzis/view.cpp so we have a search history needs a fix to handle regexps like : my_text\nbeginning_of_new_line as our searches are for now line by line based ...

--- libyzis/commands.cpp
+++ libyzis/commands.cpp
@@ -80,6 +80,7 @@ void YZCommandPool::initPool() {
 	NEW_VIEW_COMMAND("(\".)?([0-9]*)(y.|Y)",&YZView::copy,true,true,false,true);
 	NEW_VIEW_COMMAND("(\".)?(p|P)",&YZView::paste,true,false,false,true);
 	NEW_BUFF_COMMAND("u",&YZBuffer::undoLast,true,false,false,false);
+	NEW_VIEW_COMMAND("/",&YZView::gotoSearchMode,true,false,false,false);
 }
 
 void YZCommandPool::execCommand(YZView *view, const QString& inputs, int * /* error */ ) {

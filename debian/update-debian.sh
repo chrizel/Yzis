0ec59309fa4ef8024626367ac9b1ad12aa007788
Mickael Marchand <mikmak@freenux.org>
2004-04-09 19:24:51 +0000

    a little script to update debian/ 's dir automatically

---
+++ debian/update-debian.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+# this script must be run from the debian/ directory
+#
+
+mv -f changelog changelog.old;
+
+#cat templates/changelog | sed -e 's/__DATE__/`date +%d%m%Y`/' | sed -e 's/__LONGDATE__/`date -R`/' > changelog;
+perl -pe "s/__DATE__/`date +%d%m%Y`/" templates/changelog > changelog.tmp;
+perl -pe "s/__LONGDATE__/`date -R`/" changelog.tmp > changelog;
+rm -f changelog.tmp;
+cat changelog.old >> changelog;
+rm -f changelog.old;
+

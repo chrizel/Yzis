2165d1b6e351fa0fbdee3301bca611cefd60b89e
Mickael Marchand <mikmak@freenux.org>
2004-04-09 19:42:58 +0000

    hmm that's better

--- debian/update-debian.sh
+++ debian/update-debian.sh
@@ -6,7 +6,7 @@
 mv -f changelog changelog.old;
 
 #cat templates/changelog | sed -e 's/__DATE__/`date +%d%m%Y`/' | sed -e 's/__LONGDATE__/`date -R`/' > changelog;
-perl -pe "s/__DATE__/`date +%d%m%Y`/" templates/changelog > changelog.tmp;
+perl -pe "s/__DATE__/`date +%Y%d%m`/" templates/changelog > changelog.tmp;
 perl -pe "s/__LONGDATE__/`date -R`/" changelog.tmp > changelog;
 rm -f changelog.tmp;
 cat changelog.old >> changelog;

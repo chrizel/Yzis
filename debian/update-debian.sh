87b3d925ac13afa0fbf8ca54dc62ac278925fc54
Mickael Marchand <mikmak@freenux.org>
2004-04-09 20:05:03 +0000

    tss...

--- debian/update-debian.sh
+++ debian/update-debian.sh
@@ -5,8 +5,7 @@
 
 mv -f changelog changelog.old;
 
-#cat templates/changelog | sed -e 's/__DATE__/`date +%d%m%Y`/' | sed -e 's/__LONGDATE__/`date -R`/' > changelog;
-perl -pe "s/__DATE__/`date +%Y%d%m`/" templates/changelog > changelog.tmp;
+perl -pe "s/__DATE__/`date +%Y%m%d`/" templates/changelog > changelog.tmp;
 perl -pe "s/__LONGDATE__/`date -R`/" changelog.tmp > changelog;
 rm -f changelog.tmp;
 cat changelog.old >> changelog;

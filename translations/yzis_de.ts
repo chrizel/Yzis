d0710b33642e01ececc99f1db313963e708bd5fb
Thomas Capricelli <orzel@freehackers.org>
2004-05-02 07:36:28 +0000

    added german translations, by Benjamin Adler It seems like adding those two files is enough, is that ok ?

---
+++ translations/yzis_de.ts
@@ -0,0 +1,111 @@
+<!DOCTYPE TS><TS>
+<context>
+    <name>KYZisDoc</name>
+    <message>
+        <source>Yzis is not able to save remote files for now</source>
+        <translation>Yzis kann die Remote-Datei momentan nicht speichern</translation>
+    </message>
+    <message>
+        <source>Remote files</source>
+        <translation>Remote-Dateien</translation>
+    </message>
+</context>
+<context>
+    <name>KYZisFactory</name>
+    <message>
+        <source>Error</source>
+        <translation>Fehler</translation>
+    </message>
+</context>
+<context>
+    <name>KYZisView</name>
+    <message>
+        <source>Yzis Ready</source>
+        <translation>Yzis bereit</translation>
+    </message>
+    <message>
+        <source>-- INSERT --</source>
+        <translation>-- EINFÜGEN --</translation>
+    </message>
+    <message>
+        <source>-- REPLACE --</source>
+        <translation>-- ERSETZEN --</translation>
+    </message>
+    <message>
+        <source>Command mode</source>
+        <translation>Kommando-Modus</translation>
+    </message>
+    <message>
+        <source>-- EX --</source>
+        <translation>-- EX --</translation>
+    </message>
+    <message>
+        <source>-- REVERSE SEARCH --</source>
+        <translation>-- RÜCKWÄRTSSUCHE --</translation>
+    </message>
+    <message>
+        <source>-- SEARCH --</source>
+        <translation>-- SUCHE --</translation>
+    </message>
+</context>
+<context>
+    <name>NYZView</name>
+    <message>
+        <source>Entering Insert mode</source>
+        <translation type="obsolete">Gehe in Einfüge-Modus</translation>
+    </message>
+    <message>
+        <source>Entering Replace mode</source>
+        <translation type="obsolete">Gehe in Ersetzungs-Modus</translation>
+    </message>
+    <message>
+        <source>Entering Command mode</source>
+        <translation type="obsolete">Gehe in Kommando-Modus</translation>
+    </message>
+    <message>
+        <source>EX Mode :</source>
+        <translation type="obsolete">EX Modus:</translation>
+    </message>
+    <message>
+        <source>Reverse Search:</source>
+        <translation type="obsolete">Rückwärtssuche:</translation>
+    </message>
+    <message>
+        <source>Search mode :</source>
+        <translation type="obsolete">Suchmodus:</translation>
+    </message>
+</context>
+<context>
+    <name>YZBuffer</name>
+    <message>
+        <source>Written %1 bytes to file %2</source>
+        <translation>%1 Bytes in Datei %2 geschrieben</translation>
+    </message>
+</context>
+<context>
+    <name>YZExExecutor</name>
+    <message>
+        <source>Please specify a filename</source>
+        <translation>Bitte geben Sie einen Dateinamen an</translation>
+    </message>
+    <message>
+        <source>One file is modified ! Save it first ...</source>
+        <translation>Eine Datei ist ungespeichert, bitte speichern Sie diese vorher.</translation>
+    </message>
+</context>
+<context>
+    <name>YZView</name>
+    <message>
+        <source>All</source>
+        <translation>Alle</translation>
+    </message>
+    <message>
+        <source>Top</source>
+        <translation>Oben</translation>
+    </message>
+    <message>
+        <source>Bot</source>
+        <translation>Unten</translation>
+    </message>
+</context>
+</TS>

932756dfa6c1442619ed793f52e84fd2bbe6b0ca
Mickael Marchand <mikmak@freenux.org>
2004-05-10 19:51:19 +0000

    misc fixes add an example script to show internal Lua bindings

---
+++ doc/examples/example.lua
@@ -0,0 +1,11 @@
+-- call me with :source example.lua from Yzis ;)
+--
+-- delete the first line
+delete(1)
+
+insert(1,1,"See how Lua rocks !")
+
+replace(1,2,"Lua definitely rocks, we should just replace that text\nAnd we do it on multiple lines")
+
+goto(2,1)
+

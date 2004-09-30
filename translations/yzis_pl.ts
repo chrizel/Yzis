8a80d53120d45a7e0f65f646485fee6617f22ce8
Mickael Marchand <mikmak@freenux.org>
2004-09-30 18:24:22 +0000

    new polish translation
    thanks Michal Slonina !

---
+++ translations/yzis_pl.ts
@@ -0,0 +1,139 @@
+<!DOCTYPE TS><TS>
+<context>
+    <name>KYZisDoc</name>
+    <message>
+        <source>Yzis is not able to save remote files for now</source>
+        <translation>Yzis nie może zapisać plików znajdujących się na innym komputerze</translation>
+    </message>
+    <message>
+        <source>Remote files</source>
+        <translation>Pliki znajdujące się na innym komputerze</translation>
+    </message>
+</context>
+<context>
+    <name>KYZisFactory</name>
+    <message>
+        <source>Error</source>
+        <translation>Błąd</translation>
+    </message>
+</context>
+<context>
+    <name>YZBuffer</name>
+    <message>
+        <source>Recover</source>
+        <translation>Odzyskać</translation>
+    </message>
+    <message>
+        <source>A swap file was found for this file, it was presumably created because your computer or yzis crashed, do you want to start the recovery of this file ?</source>
+        <translation>Znaleziono plik tymczasowy, został on stworzony prawdopodobnie dlatego, że twój komputer lub yzis zawiesił się, czy chcesz odzyskać ten plik?</translation>
+    </message>
+    <message>
+        <source>Written %1 bytes to file %2</source>
+        <translation>Zapisano %1 byte&apos;ów do pliku %2</translation>
+    </message>
+</context>
+<context>
+    <name>YZExExecutor</name>
+    <message>
+        <source>One file is modified ! Save it first ...</source>
+        <translation>Jeden plik jest zmieniony ! Zapisz go najpierw ...</translation>
+    </message>
+    <message>
+        <source>Please specify a filename</source>
+        <translation>Proszę podaj nazwę pliku</translation>
+    </message>
+    <message>
+        <source>Invalid option given : </source>
+        <translation>Podano nieprawidłową opcję :</translation>
+    </message>
+    <message>
+        <source>This option cannot be switched this way, this is a boolean option.</source>
+        <translation>Ta opcja nie może być ustawiona w ten sposób, to jest wartość tak/nie.</translation>
+    </message>
+    <message>
+        <source>This option is a global option which cannot be changed with setlocal</source>
+        <translation>Ta opcja jest opcją globalną, i nie może być zmieniona za pomocą setlocal</translation>
+    </message>
+    <message>
+        <source>Invalid option given</source>
+        <translation>Podano nieprawidłową opcję</translation>
+    </message>
+</context>
+<context>
+    <name>YZExLua</name>
+    <message>
+        <source>The file %1 could not be found in standard directories</source>
+        <translation>Plik %1 nie został znaleziony w standardowych katalogach</translation>
+    </message>
+</context>
+<context>
+    <name>YZSwapFile</name>
+    <message>
+        <source>Warning, the swapfile could not be opened maybe due to restrictive permissions.</source>
+        <translation>Uwaga, nie można otworzyć pliku tymczasowego, prawdopodobnie z powodu zbyt restrykcyjnych praw dostępu.</translation>
+    </message>
+    <message>
+        <source>Warning, the swapfile could not be created maybe due to restrictive permissions.</source>
+        <translation>Uwaga, nie można stworzyć pliku tymczasowego, możliwe że powodem są zbyt restrykcyjne prawa dostępu.</translation>
+    </message>
+    <message>
+        <source>The swap file could not be opened, there will be no recovering for this file, you might want to check permissions of files.</source>
+        <translation>Nie można otworzyć pliku tymczasowego, nie będzie można odzyskać tego pliku, możesz spróbować sprawdzić prawa dostępu do plików.</translation>
+    </message>
+</context>
+<context>
+    <name>YZView</name>
+    <message>
+        <source>[ Insert ]</source>
+        <translation>[ Wklej ]</translation>
+    </message>
+    <message>
+        <source>[ Replace ]</source>
+        <translation>[ Zamień ]</translation>
+    </message>
+    <message>
+        <source>[ Awaiting Command ]</source>
+        <translation>[ Oczekuje na Rozkaz ]</translation>
+    </message>
+    <message>
+        <source>[ Ex ]</source>
+        <translation>[ Ex ]</translation>
+    </message>
+    <message>
+        <source>[ Search ]</source>
+        <translation>[ Szukaj ]</translation>
+    </message>
+    <message>
+        <source>[ Open ]</source>
+        <translation>[ Otwórz ]</translation>
+    </message>
+    <message>
+        <source>[ Visual ]</source>
+        <translation>[ Wizualny ]</translation>
+    </message>
+    <message>
+        <source>[ Visual Line ]</source>
+        <translation>[ Wizualna Linia ]</translation>
+    </message>
+    <message>
+        <source>Yzis Ready</source>
+        <translation>Yzis Gotowy</translation>
+    </message>
+    <message>
+        <source>All</source>
+        <translation>Wszystko</translation>
+    </message>
+    <message>
+        <source>Top</source>
+        <translation>Góra</translation>
+    </message>
+    <message>
+        <source>Bot</source>
+        <translation>Doł</translation>
+    </message>
+    <message>
+        <source>To use the Qt printer, you need to have an X11 DISPLAY set and running, you should try pslib in console mode</source>
+        <translation>Aby używać drukarki Qt, musisz mieć otwarty i ustawiony ekran X11, pod konsolą tekstową możesz spróbować pslib&apos;a</translation>
+    </message>
+</context>
+</TS>

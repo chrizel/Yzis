7030610c23ec0807ac489ee51dd519c00c8d177e
Loic Paulevé <panard@backzone.net>
2004-11-07 17:56:24 +0000

    ooops :)

---
+++ search.h
@@ -0,0 +1,84 @@
+/*  This file is part of the Yzis libraries
+ *  Copyright (C) 2004 Loic Pauleve <panard@inzenet.org>
+ *  Mickael Marchand <marchand@kde.org>
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Library General Public
+ *  License as published by the Free Software Foundation; either
+ *  version 2 of the License, or (at your option) any later version.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Library General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Library General Public License
+ *  along with this library; see the file COPYING.LIB.  If not, write to
+ *  the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ *  Boston, MA 02111-1307, USA.
+ **/
+
+/**
+ * $Id$
+ */
+
+#ifndef YZ_SEARCH_H
+#define YZ_SEARCH_H
+
+#include "cursor.h"
+#include "view.h"
+
+class YZView;
+class YZCursor;
+
+/**
+ * YZSearch : 
+ *	Searches are common to all buffers and views. 
+ *	When doing a search if hlsearch is set, we have to highlight
+ *	all matching strings on all views of each buffers.
+ */
+
+class YZSearch {
+
+	public :
+		YZSearch();
+		~YZSearch();
+
+		/**
+		 * search after current cursor position
+		 */
+		YZCursor forward( YZView* mView, const QString& pattern, bool* found );
+
+		/**
+		 * search before current cursor position
+		 */
+		YZCursor backward( YZView* mView, const QString& pattern, bool* found );
+
+		/**
+		 * replay search forward
+		 */
+		YZCursor replayForward( YZView* mView, bool* found, bool skipline = false );
+
+		/**
+		 * replay search backward
+		 */
+		YZCursor replayBackward( YZView* mView, bool* found, bool skipline = false );
+
+		/**
+		 * return current search
+		 */
+		const QString& currentSearch() const;
+
+
+	private :
+
+		void setCurrentSearch( const QString& pattern );
+		YZCursor doSearch( YZView* mView, const QString& pattern, bool reverse, bool replay, bool skipline, bool* found );
+		void highlightSearch( YZView* mView, YZSelectionMap searchMap );
+
+		QString mCurrentSearch;
+
+};
+
+#endif
+

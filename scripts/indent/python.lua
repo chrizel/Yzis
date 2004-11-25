8ab791cd1e24cbb72b696992cd900fc9bee4c1cf
Loic Paulevé <panard@backzone.net>
2004-11-25 22:06:35 +0000

    try to do indent for python

---
+++ scripts/indent/python.lua
@@ -0,0 +1,18 @@
+-- Indentation plugin for python
+-- Author : Loic Pauleve <panard@inzenet.org>
+
+function Indent_Python( nbNextTabs, nbNextSpaces, nbPrevTabs, nbPrevSpaces, prevLine, nextLine )
+	local nbtabs = nbPrevTabs
+	local nbspaces = nbPrevSpaces
+
+	local result = string.rep( "\t", nbtabs )..string.rep( " ", nbspaces )
+	
+	if string.find( prevLine, ":%s*$" ) then
+		result = result.."\t"
+	end
+
+	return result
+end
+
+connect( "INDENT_ON_ENTER", "Indent_python" )
+

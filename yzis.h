ddfbe31fd7d295559033c97f821149f8dc603d1f
Mickael Marchand <mikmak@freenux.org>
2005-01-18 23:55:21 +0000

    bump version ;)

--- yzis.h
+++ yzis.h
@@ -49,7 +49,7 @@
 #define VERSION_CHAR_STATE3 "(preview release - Use for testing only)"
 //and change/use this one in the code
 #define VERSION_CHAR_ST VERSION_CHAR_STATE2
-#define VERSION_CHAR_DATE "SVN>2005-01-02"
+#define VERSION_CHAR_DATE "SVN>2005-01-19"
 
 #if QT_VERSION < 0x040000
 #define qMax QMAX

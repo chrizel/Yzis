ed01d53f85f74c6aa2e4355b1af3b09216be9a4c
Mickael Marchand <mikmak@freenux.org>
2005-02-10 19:50:57 +0000

    it was really time to fix these bugs :)

--- scripts/clevertab.lua
+++ scripts/clevertab.lua
@@ -5,10 +5,10 @@
 function cleverTab(key) 
 	local str = line(winline())
 --	debug("("..str..")")
-	if string.sub(str, -1) == " " or string.sub(str,-1) == "\t" or str == "" then
+	if string.sub(str, -1) == " " or string.sub(str,-1) == "\t" or str == "" or wincol() == 1 then
 		return "<TAB>"
 	else
-		return "<CTRL>x<CTRL>n"
+		return "<CTRL>p"
 	end
 	return ""
 end 

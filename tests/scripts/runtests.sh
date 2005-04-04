4c126b2b0f537b18134df0948e6572307f6020bc
Mickael Marchand <mikmak@freenux.org>
2005-04-04 20:10:09 +0000

    make sure it's clean :)

--- tests/scripts/runtests.sh
+++ tests/scripts/runtests.sh
@@ -8,4 +8,16 @@ if [ -n "$1" ];
 then
     testname=$1
 fi
+if [ -f ~/.yzis/yzis.conf ]; then
+	mv ~/.yzis/yzis.conf ~/.yzis/yzis.conf.old
+fi
+if [ -f ~/.yzis/init.lua ]; then
+	mv -f ~/.yzis/init.lua ~/.yzis/init.lua.old
+fi
 LANG=C kyzis -c ":source $testname <ENTER><ESC>:qall!<ENTER>"
+if [ -f ~/.yzis/yzis.conf.old ]; then
+	mv ~/.yzis/yzis.conf.old ~/.yzis/yzis.conf
+fi
+if [ -f ~/.yzis/init.lua.old ]; then
+	mv -f ~/.yzis/init.lua.old ~/.yzis/init.lua
+fi

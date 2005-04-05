27be762e913d9ab496b6713f6d0097464f49bb33
Loic Paulevé <panard@backzone.net>
2005-04-05 22:26:44 +0000

    expand ~ in YZExLua::source CCMAIL: 119-done@bugs.yzis.org

--- ex_lua.cpp
+++ ex_lua.cpp
@@ -243,6 +243,12 @@ QString YZExLua::source( YZView *, const QString& args, bool canPopup ) {
 #endif
 	if ( !filename.endsWith( ".lua" ) )
 		filename += ".lua";
+	if ( filename.startsWith("~") ) // expand ~
+#if QT_VERSION < 0x040000
+		filename = QDir::homeDirPath() + filename.mid( 1 );
+#else
+		filename = QDir::homePath() + filename.mid( 1 );
+#endif
 	yzDebug() << "looking filename : " << filename << endl;
 	QStringList candidates;
 	candidates << filename 

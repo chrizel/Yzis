932c1a05ff5be8a1585e167af5a47be4ac5054c1
Loic Paulevé <panard@backzone.net>
2005-04-06 17:22:37 +0000

    move testStickyCol to test_movements

--- tests/scripts/test_all.lua
+++ tests/scripts/test_all.lua
@@ -4,7 +4,6 @@ require('test_lua_binding')
 require('test_movements')
 require('test_excommands')
 require('test_bugs1')
-require('test_goto')
 
 -- LuaUnit:run('TestLuaBinding:test_setline') -- will execute only one test
 -- LuaUnit:run('TestMovements') -- will execute only one class of test
--- tests/scripts/test_movements.lua
+++ tests/scripts/test_movements.lua
@@ -298,6 +298,40 @@ TestMovements = {} --class
 	    --cleanup
 		clearBuffer()
 	end
+
+	function TestMovements:testStickyColumn()
+		clearBuffer()
+		insertline(1, "line1 line1 line1 line1 line1")
+		insertline(2, "		line2 line2 line2 line2")
+		printBufferContent()
+		goto(1,1)
+		assertPos(1,1)
+		sendkeys("2lj")
+		assertScrPos(2,1)
+		assertPos(2,1)
+		sendkeys("k")
+		assertScrPos(1,3)
+		assertPos(1,3)
+		sendkeys("8lj")
+		assertScrPos(2,9)
+		assertPos(2,2)
+		sendkeys("k")
+		assertScrPos(1,11)
+		assertPos(1,11)
+		sendkeys("8l")
+		assertPos(1,19)
+		sendkeys("j")
+		assertScrPos(2,19)
+		assertPos(2,5)
+		sendkeys("k")
+		assertScrPos(1,19)
+		sendkeys("j$")
+		assertPos(2,25)
+		assertScrPos(2,39)
+		sendkeys("k")
+		assertPos(1,29)
+		assertScrPos(1,29)
+	end
 	
 
 if not _REQUIREDNAME then

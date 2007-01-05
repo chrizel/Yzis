e51a100429dddf48934f9bae4f0f57d9d3a81f9f
Philippe Fremy <phil@freehackers.org>
2007-01-05 16:13:08 +0000

    Debug is now more powerful: - you can use yzDebug(area).sprintf() - you can customise area debug level by a rcfile - you can customise area debug level by argc/argv - area is prepended to debug log, for easier reading - convenient macro qp() for qPrintable() - convenient macro HERE() that contains the name and signature of the function and a line number - convenient macro LOCATION() that conatins the filename and line number
    Next step is to use the debugging power in yzis.

--- libyzis/debug.h
+++ libyzis/debug.h
@@ -55,64 +55,171 @@ class QStringList;
 
 #define FILENAME_DEBUGRC ".yzdebugrc"
 
+/** Log system used for debugging.
+  *
+  * Our debugging system supports 4 levels:
+  * - YZ_DEBUG_LEVEL: shows debugging output, warning and errors
+  * - YZ_WARNING_LEVEL: shows warnings and errors, debug output is disabled
+  * - YZ_ERROR_LEVEL: shows only errors, debug and warnings are disabled.
+  * - YZ_FATAL_LEVEL: shows only fatal errors
+  * 
+  *
+  * The debug output may be:
+  * - sent to a specified filename
+  * - sent to a file in the temp directory
+  * - sent to the stderr
+  * - sent to the stdout
+  * This is controlled by setDebugOutput()
+  *
+  * Moreover, in order to ease the debugging process, it is possible to define
+  * areas, and to assign an individual debugging level to each of them with
+  * setAreaLevel().
+  *
+  * This allows for example to have the rendering code only display errors but
+  * keep all the debugging output of the scripting engine. Or the opposite.
+  *
+  * Because no two developers will need the same configuration, the debug
+  * level and areas can be configured dynamically through two methods:
+  * - command line arguments: see parseArgv()
+  * - .yzdebugrc file in the current directory: see parseRcfile()
+  *
+  * The calls to parseArgv() and parseRcfile() need to be explicit, the
+  * framework does not do any automatic action.
+  *
+  * By default, the debug framework will start with no areas, debug level set
+  * to debug in debug mode (DEBUG symbol defined) or warning in release mode
+  * (DEBUG symbol not defined). 
+  *
+  * The default output is a file in the /tmp directory named yzisdebug.log on
+  * windows and yzisdebug-[your user name].log on unix.
+  *
+  * The framework gives a very flexible approach for debugging.
+  *
+  * When you are writing code, you should first define an area for your code
+  * and then use the stream yzDebug(area) to output your debug information.
+  *
+  * My recommendation is to have a define at the beginning of your source file
+  * to abbreviate the debug writing command:
+  * \code
+  * #define dbgHl() (yzDebug("SyntaxHighlighting"))
+  *
+  * dbgHl() << "some debug intput";
+  * \endcode
+  *
+  */
 class YZDebugBackend {
 public:
 	~YZDebugBackend();
+
+    /** The singleton constructor */
 	static YZDebugBackend * instance();
 
-	/** write data to the debug backend */
+	/** Write data to the debug backend. 
+      *
+      * Note that Qt provides a macro qPrintable that makes mostly
+      * anything printable as a const char * . This can be convenient.
+      *
+      * \param level the debugging level
+      * \param area  the area
+      * \param data  the data to display
+      */
 	void flush( int level, const QString& area, const char * data );
 
-	/** All debugging info under level will not be printed.
-	  * setDebugLevel( YZ_DEBUG_LEVEL ) will log all debug output.
+	/** Set debugging level.
+      *
+      * \param level can YZ_DEBUG_LEVEL, YZ_ERROR_LEVEL or YZ_FATAL_LEVEL
 	  */
 	void setDebugLevel( int level ) { _level = level; }
 
+    /** Current debugging level */ 
 	int debugLevel() const { return _level; }
 
-	/** All debug will be logged to the open file descriptor file.
-	  * stdout and stderr are perfectly valid file descriptors. */
+	/** All debug will be logged to the open file descriptor \p file.
+      *
+	  * stdout and stderr are perfectly valid file descriptors for this.
+      */
 	void setDebugOutput( FILE * file );
 
-	/** Same as above, but just specifies the file name */
+	/** Same as above, but just specifies a file name that 
+      * will be opened instead of a file descriptor. 
+      */
 	void setDebugOutput( const QString& );
 
-	/** Enable/Disable the log output of area */
-	void enableDebugArea( const QString& area, bool enabled ) {
-		_areaOutput[area] = enabled;
+	/** Set the debug \p level of an \p area 
+      *
+      * Once the debug level of an area has been set, it no longer
+      * obeys the global debugging level.
+      *
+      * You can remove an area level with removeArea()
+      */
+	void setAreaLevel( const QString& area, int level ) {
+		_areaLevel[area] = level;
 	}
 
-	/** Return whether an area is enabled. All area are enalbed by default
-	  */
-	bool isAreaEnabled( const QString& area ) const {
-		if (_areaOutput.contains( area )) {
-			return _areaOutput[area];
-		} else {
-			return true;
-		}
+	/** Returns the debugging level of an \p area. 
+      *
+      * If the area has not been assigned an individual debugging level,
+      * returns the global level.
+      */
+	int areaLevel( const QString& area ) const {
+        return _areaLevel.value(area, debugLevel());
 	}
 
-	/** Read the file to enable/disable some debug area.
+    /** Remove the individual debugging level of this area.
+      *
+      * The area now obeys the global debugging level.
+      */
+    void removeArea(const QString & area) {
+        _areaLevel.remove( area );
+    }
+
+	/** Remove all debug areas. 
+      *
+      * Debug value returns to the global value.
+      */
+	void clearArea() { _areaLevel.clear(); }
+
+	/** Read a rcfile to adjust area output and debug level
+      *
 	  * The syntax is:
-	  * enable:area -> area is enabled
-	  * disable:area -> area is disabled
-	  *
-	  * no whitespace should be added at the beginning of the line
+      * "level" ":" "debug" | "warning" | "error"
+      *  [area]   ":" "debug" | "warning" | "error"
+      * "output" ":" [filename]
 	  **/
 	void parseRcfile(const char * filename);
 
-	/** Reset all area to their default enabled value */
-	void clear() { _areaOutput.clear(); }
+    /** Parse default rc file.
+      *
+      * The default rc file is a file named .yzdebugrc located in the
+      * directory where yzis is executed.
+      */
+	void parseRcfile();
 
-	/** Reset the object to its initial state: clear all the disabled/enabled
-	  * area and parse the rc file again */
-	void init();
+
+    /** Parses argc/argv to check for debug directives.
+      *
+      * Allowed directives are:
+      * --level=debug|warning|error|fatal
+      * --area-level=[area name],debug|warning|error|fatal
+      *
+      * Example:
+      * nyzis --level=warning --area-level=LuaEngine,debug --area-level=YZSession,debug
+      */
+    void parseArgv(int & argc, char ** argv);
+
+    /** Returns a string describing the YZDebugBackend current configuration
+      */
+    QString toString();
 
 private:
+    void init();
+
 	YZDebugBackend();
 	static YZDebugBackend * _instance;
 
-	QMap<QString,bool> _areaOutput;
+	QMap<QString,int> _areaLevel;
+	QMap<QString,int> _levelByName;
+	QMap<int,QString> _nameByLevel;
 	int _level;
 	FILE * _output;
 };
@@ -154,6 +261,8 @@ class YZIS_EXPORT YZDebugStream {
 			return ( *f )( *this );
 		}
 
+        /** Convenient sprintf function on debug streams */
+        void sprintf( const char * fmt, ... );
 
 		void flush();
 
@@ -177,5 +286,15 @@ YZIS_EXPORT YZDebugStream yzFatal( const char * area = "" );
 #define YZASSERT_MSG( assertion, msg ) { if (! (assertion) ) { yzError() << QString("%1:%2 assertion '%3' failed : %4\n").arg(__FILE__).arg( __LINE__).arg(#assertion).arg( msg ); } }
 #define YZASSERT( assertion ) YZASSERT_MSG( assertion, "" )
 #define YZASSERT_EQUALS( a, b ) { if (a != b) { yzError() << QString("%1:%2 - %3 == %4 failed : '%5' != '%6'\n").arg(__FILE__).arg( __LINE__).arg(#a).arg(#b).arg(a).arg(b); } }
+
+/** qPrintable shortcut, convenient when debugging. */
+#define qp(s)   qPrintable(s)
+
+/** Function + line, very convenient when debugging */
+#define HERE() qp(QString("%1:%2 ").arg(__PRETTY_FUNCTION__).arg(__LINE__))
+
+/** File + line location, convenient when debugging */
+#define LOCATION() qp(QString("%1:%2 ").arg(__FILE__).arg(__LINE__))
+
 
 #endif /* YZ_DEBUG_H */

3c62de826dc84d880367a02c214b302aceb60d85
Philippe Fremy <phil@freehackers.org>
2007-02-12 14:48:04 +0000

    - allow to run tests individually on the command line - script to copy the executables and run the unit tests - on windows, install the library in the bin directory as well, so that one can execute it (else, you get an error message about missing dll) - change name of executable

--- tests/unittest/main.cpp
+++ tests/unittest/main.cpp
@@ -2,14 +2,40 @@
 #include "testDebug.h"
 #include "testColor.h"
 
+/** 
+  * How to use it:
+  *
+  * Run an individual class test: yzistest <TestClassName>
+  * Run all unit tests: yzistest
+  *
+  * To write a test, see the Qt documentation. 
+  *
+  */
 int main( int argc, char * argv[] )
 {
-    TestYZDebugBackend testYZDebugBackend;
-    QTest::qExec( &testYZDebugBackend, argc, argv );
+    QMap<QString,bool> runMe;
+    bool runAll = false;
+    int fakeArgc = 1;
+    char * fakeArgv[] = { "yzistest.exe", NULL };
 
-    printf("\n");
+    if (argc == 1) {
+        runAll = true;
+    } else {
+        for( int i=1; i<argc; i++) {
+            runMe[argv[i]] = true;
+        }
+    }
 
-    TestColor testColor;
-    QTest::qExec( &testColor, argc, argv );
+    if (runAll || runMe.contains("TestYZDebugBackend")) {
+        TestYZDebugBackend testYZDebugBackend;
+        QTest::qExec( &testYZDebugBackend, fakeArgc, fakeArgv );
+        printf("\n");
+    }
+
+    if (runAll || runMe.contains("TestColor")) {
+        TestColor testColor;
+        QTest::qExec( &testColor, fakeArgc, fakeArgv );
+        printf("\n");
+    }
 }
 
---
+++ tests/unittest/run_unittests.sh
@@ -0,0 +1,48 @@
+#!/bin/sh 
+# Usage:
+# ./runtests.sh [testname]
+#
+# Copies from the build tree the latest versions of testyzis
+# Then execute the test name provided on the command line.
+#
+
+export TMP="d:/tmp"
+export TEMP="d:/tmp"
+
+if [ -n "$CYGWIN" ]; 
+then
+    _exe=".exe"
+else
+    _exe=""
+fi
+
+
+echo "Copying yzis files"
+cp -a ../../build-cmake/libyzis/libyzis* . 
+cp -a ../../build-cmake/tests/unittest/yzistest$_exe .
+
+YZIS=testyzis$_exe
+
+case "x$1" in
+    ("xgdb")
+        shift
+        YZIS="gdb"
+        ;;
+esac
+
+if [ -n "$*" ]; 
+then
+    testname=$*
+fi
+
+echo "Testing with: $YZIS $testname"
+
+if [ "$YZIS" == "gdb" ];
+then
+    # execution in gdb
+    LANG=C gdb yzistest$_exe $testname
+else
+    # normal execution
+    LANG=C ./yzistest$_exe $testname
+fi
+

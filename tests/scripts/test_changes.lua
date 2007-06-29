bab5d794ebf9d20177c8d4fbe7fd2cd643c18a34
Erlend Hamberg <hamberg@stud.ntnu.no>
2007-06-29 17:15:31 +0000

    r4697@hamberg-laptop:  hamberg | 2007-06-25 15:24:54 +0200  Check the d commando with various movements

--- tests/scripts/test_changes.lua
+++ tests/scripts/test_changes.lua
@@ -35,6 +35,51 @@ TestChanges = {} --class
 	clearBuffer()
     end
 
+    function TestChanges:test_d()
+	assertEquals(bufferContent(), "")
+	assertPos(1,1)
+	insertline(1, "WORD")
+	assertPos(1,4)
+	goto(1,1)
+	sendkeys("d$")
+	assertEquals(bufferContent(), "")
+	assertPos(1,1)
+	insertline(1, "WORD")
+	assertPos(1,4)
+	sendkeys("dd")
+	assertEquals(bufferContent(), "")
+	assertPos(1,1)
+	insertline(1, "LINE 1")
+	assertPos(1,6)
+	insertline(2, "LINE 2")
+	assertPos(2,6)
+	sendkeys("dd")
+	assertEquals(bufferContent(), "LINE 1")
+	assertPos(1,1)
+	sendkeys("dd")
+	assertEquals(bufferContent(), "")
+
+	assertPos(1,1)
+	insertline(1, "WORD")
+	goto(2,1)
+	sendkeys("D")
+	assertEquals(bufferContent(), "W")
+	assertPos(1,1)
+	sendkeys("D")
+	assertEquals(bufferContent(), "")
+
+	assertPos(1,1)
+	insertline(1, "LINE 1")
+	assertPos(1,6)
+	insertline(2, "LINE 2")
+	assertPos(2,6)
+	insertline(3, "LINE 3")
+	assertPos(3,6)
+	goto(1,1)
+	sendkeys("dG")
+	assertEquals(bufferContent(), "")
+    end
+
 if not _REQUIREDNAME then
     -- LuaUnit:run('TestLuaBinding:test_setline') -- will execute only one test
     -- LuaUnit:run('TestLuaBinding') -- will execute only one class of test

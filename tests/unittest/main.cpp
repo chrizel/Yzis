27affa888a652068d6e52ca5459cf77b90a20bec
Philippe Fremy <phil@freehackers.org>
2007-07-20 19:54:59 +0000

    - unit test are separeted in multiple test - need to initialise result

--- tests/unittest/main.cpp
+++ tests/unittest/main.cpp
@@ -32,7 +32,7 @@ int main( int argc, char * argv[] )
     QMap<QString,QString> runMe;
     QStringList myArgv;
     bool runAll = false;
-    int result,fakeArgc = 1;
+    int result=0,fakeArgc = 1;
     char * fakeArgv[] = { NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL };
     QRegExp reTestName( "(\\w+)(::(\\w+))?(\\(\\))?" );
 
@@ -67,7 +67,7 @@ int main( int argc, char * argv[] )
         fakeArgv[fakeArgc] = NULL; \
  \
         TestName TestName##inst ; \
-        result = QTest::qExec( & TestName##inst, fakeArgc, fakeArgv ); \
+        result += QTest::qExec( & TestName##inst, fakeArgc, fakeArgv ); \
         printf("\n"); \
     } \
 

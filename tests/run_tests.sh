1fbda7eb3687976211daf9fc62307917f84ee304
Philippe Fremy <phil@freehackers.org>
2007-08-17 10:02:17 +0000

    one test script to rule them all

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -6,17 +6,22 @@ echo "Running unit tests"
 (cd unittest && ./run_unittests.sh)
 nb_unittest_failed=$?
 
-echo "Running lua tests"
+echo "Running lua tests (libyzisrunner)"
 (cd scripts && ./run_scripttests.sh)
-nb_scripttest_failed=$?
+nb_scripttest_libyr_failed=$?
+
+echo "Running lua tests (qyzis)"
+(cd scripts && ./run_scripttests.sh qyzis)
+nb_scripttest_qyzis_failed=$?
 
 echo
 echo "======================[ Global Status ]================================"
 
-nb_failed=$(( $nb_unittest_failed + $nb_scripttest_failed ))
+nb_failed=$(( $nb_unittest_failed + $nb_scripttest_libyr_failed + $nb_scripttest_qyzis_failed ))
 
-echo "Unit tests: $nb_unittest_failed failed tests"
-echo "Lua tests:  $nb_scripttest_failed failed tests"
+echo "               Unit tests: $nb_unittest_failed failed tests"
+echo "Lua tests (libyzisrunner): $nb_scripttest_libyr_failed failed tests"
+echo "        Lua tests (qyzis): $nb_scripttest_qyzis_failed failed tests"
 echo
 echo "Total: $nb_failed failed tests"
 

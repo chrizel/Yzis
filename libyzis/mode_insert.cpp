386959eff1d974b8074a9c7dc453f7e0a7a51184
Thomas Capricelli <orzel@freehackers.org>
2008-06-15 15:18:59 +0200

    fix the test "test_bugs1.lua" that has been failing for too long. The problem had been triggered by the factorisation of commit
        changeset:   3864:d4211f7daff7
        user:        Tim Northover <T.P.Northover@sms.ed.ac.uk>
        date:        Thu Sep 13 14:15:34 2007 +0000
        summary:     Make YModeInsert a subclass of YModeCommand to share code.

--- libyzis/mode_insert.cpp
+++ libyzis/mode_insert.cpp
@@ -81,7 +81,7 @@ void YModeInsert::initCommandPool()
     commands.append( new YCommand(YKeySequence("<BS>"), (PoolMethod)&YModeInsert::backspace) );
     commands.append( new YCommand(YKeySequence("<C-h>"), (PoolMethod)&YModeInsert::backspace) );
     commands.append( new YCommand(YKeySequence("<ENTER>"), (PoolMethod)&YModeInsert::commandEnter) );
-    commands.append( new YCommand(YKeySequence("<DELETE>"), &YModeCommand::deleteChar) );
+    commands.append( new YCommand(YKeySequence("<DELETE>"), (PoolMethod)&YModeInsert::deleteChar) );
     
     // Motions don't need special processing in insert mode, so shove them
     // on the back of the command list
